267044f512c012d20d5b73eb9d3df906e88fd416
Cian <creepey.jr@gmail.com>
2026-01-24 20:50:36 +0000

    creation of java task

---
+++ lab2/PrimeNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the upper limit
+        long limit = scanner.nextInt();
+
+        // Counter of prime number
+        long cnt = 0;
+
+        // Write your code
+        ...
+
+        // Print the results
+        System.out.println(cnt);
+    }
+
+    // Function to check if it is prime
+    private static boolean isPrime(long num) {
+
+    }
+}
