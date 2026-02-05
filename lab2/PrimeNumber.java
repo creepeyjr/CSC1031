97c444807d25b00e61e9533aa2419a80520ff445
Cian <creepey.jr@gmail.com>
2026-02-05 15:24:12 +0000

    works, but must be faster and reuploaded

--- lab2/PrimeNumber.java
+++ lab2/PrimeNumber.java
@@ -11,16 +11,40 @@ public class PrimeNumber {
         long cnt = 0;
 
         // Write your code
-        
+        // for loop through numbers till limit
+        for (int i = 1; i <= limit; i ++) {
+            // on every ideration, call function and check if it returns true
+            if (isPrime(i) == true) {
+                cnt++;
+            }
+        System.err.println(cnt);
+        }
 
         // Print the results
         System.out.println(cnt);
     }
 
-    /* 
+    
     // Function to check if it is prime
     private static boolean isPrime(long num) {
-        pass;
+        
+        // iterate through all numbers from 2 to n-1, and
+        // for every number, check if it divides n.
+        // if any number that divides, return false
+
+        // corner case, if the number is 1 or less
+        if (num <= 1) {
+            return false;
+        }
+
+        // Check from 2 to n-1
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
-    */
+    
 }
