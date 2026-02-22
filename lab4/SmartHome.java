a77ea6b535375fe49be89a691580c3b2c216ed37
Cian <creepey.jr@gmail.com>
2026-02-22 13:48:27 +0000

    Finishing up Part 1 of the question.

--- lab4/SmartHome.java
+++ lab4/SmartHome.java
@@ -4,7 +4,34 @@ package lab4;  // What is this bruh??
 import java.util.ArrayList;
 import java.util.List;
 
+// I will genuinely kill myself
 
 public class SmartHome {
-    
+    // Fuck this chud class
+}
+
+// Part 1 : Creating of Appliance class
+class Appliance {
+    // Whole load of Attributes!!
+    private final int id;
+    private String brand;
+    private double powerConsumption;
+    private boolean isOn;
+    private static int nextId = 1;
+
+    // Constructor
+    public Appliance(String brand, double powerConsumption) {
+        // Responsibilities
+        this.id = nextId;
+        nextId++;
+        this.brand = brand;
+        this.powerConsumption = powerConsumption;
+        this.isOn = false;
+    }
+
+    // Required Methods
+    public int getId() {
+        id = 
+    }
+
 }
